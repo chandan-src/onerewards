dc61959b50636b585aca7956c51508826b3f9035
chandan-src <chandan@asymmetri.in>
2025-03-26 23:14:40 +0530

    build4

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-import { clsx, type classnameValue } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: classnameValue[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
